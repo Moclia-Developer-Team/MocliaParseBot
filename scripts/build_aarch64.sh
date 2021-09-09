c9a42beb5d2ba19306984cb273eac367848e0ccd
STASWIT <wubaiyuan@outlook.com>
2021-09-09 13:42:13 +0800

    test: ci

--- scripts/build_aarch64.sh
+++ scripts/build_aarch64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "creating cmake cache"
 cd ./build/aarch64
-cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release ../../
+cmake -DCMAKE_TOOLCHAIN_FILE=../../cmakeBuild/crossBuild-aarch64.cmake ../../
 echo "build elf"
 make -j20
--- scripts/prepareBuildSystem_aarch64.sh
+++ scripts/prepareBuildSystem_aarch64.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
+echo "preparing build system"
+sudo apt update
+sudo apt install -y binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 echo "preparing vcpkg"
 sudo chmod 777 ~/vcpkg/vcpkg
-~/vcpkg/vcpkg install cpr nlohmann-json fmt 
+~/vcpkg/vcpkg install cpr:arm64-linux nlohmann-json:arm64-linux fmt:arm64-linux 
 echo "init submodule"
-git submodule --init
+git submodule init
 git submodule update
 echo "creating folder"
 mkdir build
