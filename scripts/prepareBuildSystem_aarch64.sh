262c3e0ac0d99b39000b2851be40887545ac5b6b
STASWIT <wubaiyuan@outlook.com>
2021-09-09 12:15:39 +0800

    test: ci

--- scripts/prepareBuildSystem_aarch64.sh
+++ scripts/prepareBuildSystem_aarch64.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env sh
-echo "preparing build system"
-sudo apt update -y
-sudo apt upgrade -y
-
 echo "preparing vcpkg"
-git clone https://github.com/microsoft/vcpkg.git ~/vcpkg
-~/vcpkg/bootstrap-vcpkg.sh
-~/vcpkg install cpr:arm64-linux nlohmann-json:arm64-linux fmt:arm64-linux 
+~/vcpkg install cpr nlohmann-json fmt 
 
 echo "creating folder"
-cd ..
 mkdir build
 mkdir ./build/aarch64
