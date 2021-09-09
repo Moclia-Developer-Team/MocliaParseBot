4dce73a322751c641505eb52350e1ae03cec679b
STASWIT <wubaiyuan@outlook.com>
2021-09-09 12:20:12 +0800

    test: ci

--- scripts/build_aarch64.sh
+++ scripts/build_aarch64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "creating cmake cache"
 cd ./build/aarch64
-cmake -DCMAKE_TOOLCHAIN_FILE=../../cmakeBuild/crossBuild-aarch64.cmake ../../
+cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release ../../
 echo "build elf"
 make -j20
