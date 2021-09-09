c7df67d69084b4e5f096475a9ba09ae8f2370f32
STASWIT <wubaiyuan@outlook.com>
2021-09-09 10:43:43 +0800

    test: ci

--- scripts/prepareBuildSystem_aarch64.sh
+++ scripts/prepareBuildSystem_aarch64.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 echo "preparing build system"
-apt update -y
-apt upgrade -y
-apt install -y binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu libssl-dev git ninja-build
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu libssl-dev git ninja-build
 echo "building cmake"
 wget https://github.com/Kitware/CMake/releases/download/v3.21.2/cmake-3.21.2.tar.gz
 tar -zxvf cmake-3.21.2.tar.gz
 cd cmake-3.21.2
 ./configure
 make -j20
-make install
+sudo make install
 echo "preparing vcpkg"
 git clone https://github.com/microsoft/vcpkg.git ~/vcpkg
 ~/vcpkg/bootstrap-vcpkg.sh
